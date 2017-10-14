1a1acfb1831cc25cbadb9764df844c6f70edc513
sof <probablytmp@gmail.com>
2017-10-14 12:53:46 -0400

    Revert "Removed hash tables for the time being"
    This reverts commit daea30f6eaf4d0218976561e37a02a307ab3ba6a.

---
+++ src/hash.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "types.h"
+
+/*
+creates a hash table
+void *array is a pointer the said array
+char **map is the strings which map to the indexes of the array
+*/
+hash_t new_table(void *array, char **map)
+{
+	hash_t new;
+	unsigned size;
+}
